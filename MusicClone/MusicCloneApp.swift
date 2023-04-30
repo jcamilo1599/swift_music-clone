ee5328aa9951e2821e361dece8eced3de1cd9d79
Juan Camilo <99774186+jcamilo1599@users.noreply.github.com>
2023-04-30 03:05:49 -0500

    Initial Commit

---
+++ MusicClone/MusicCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicCloneApp.swift
+//  MusicClone
+//
+//  Created by Juan Camilo Marín Ochoa on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
